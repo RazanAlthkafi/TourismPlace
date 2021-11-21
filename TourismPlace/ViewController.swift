9ba0ca3dbe1b898bd9f59a66d8943367b6b590ff
RazanAlthkafi <Razan.Althkafi.med.s@tuwaiq.edu.sa>
2021-11-21 12:41:03 +0300

    Initial Commit

---
+++ TourismPlace/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TourismPlace
+//
+//  Created by Roaa aljohani on 16/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
