e6e771e696b6045be0fe6e3f06e501302411d478
RazanAlthkafi <Razan.Althkafi.med.s@tuwaiq.edu.sa>
2021-11-21 16:19:26 +0300

    start project and create home page

--- TourismPlace/ViewController.swift
+++ TourismPlace/ViewController.swift
@@ -1,17 +1,31 @@
-//
-//  ViewController.swift
-//  TourismPlace
-//
-//  Created by Roaa aljohani on 16/04/1443 AH.
-//
+
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
+    
+  
+    
+    var imageArray = [UIImage(named: "mousqe"),UIImage(named: "Masjid-al-Qiblatain"),UIImage(named: "MountUhud"),UIImage(named: "masjedquba"),UIImage(named: ""),UIImage(named: "")]
+    var textArray = ["Al Masjid Nabawi","Masjid alQiblatain","Mount Uhud","masjedquba",""]
+    @IBOutlet weak var homecollection: UICollectionView!
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = homecollection.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! imagesCell
+        cell.imageView.image =  imageArray[indexPath.row]
+        cell.namePlace.text = textArray[indexPath.row]
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        homecollection.dataSource = self
+        homecollection.delegate = self
+        
     }
 
 
